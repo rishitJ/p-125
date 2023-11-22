2eb27a11acd9a4d537cc446a6f75e5919d8fd06b
rishitJ <121947502+rishitJ@users.noreply.github.com>
2023-11-22 19:29:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function setup() 
+{
+    canvas = createCanvas(600, 500)
+    canvas.position(850, 84)
+
+    video = createCapture(VIDEO)
+    video.size(400,300)
+
+    poseNet = ml5.poseNet(video, modelloaded)
+    poseNet.on("pose", gotPoses)
+}
+function modelloaded() 
+{
+    console.log("Model Is Loaded")
+}
+function draw()
+{
+    background("white")
+    textsize(10)
+    fill("black")
+}
+function text() 
+{
+    text(Rishit, 25, 150)
+}
+function gotPoses(result) 
+{
+    if (result.length > 0) 
+    {
+        noseX = result[0].pose.nose.x
+        noseY = result[0].pose.nose.y
+
+        leftWristX = result[0].pose.leftWrist.x
+        rightWristX = result[0].pose.rightWrist.x
+
+        difference = floor(leftWristX - rightWristX)
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX)
+    }
+}
